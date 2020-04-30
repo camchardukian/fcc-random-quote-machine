757efa49169841d2bfeb2c72d52e4fd861f3dccc
Cameron Chardukian <camchardukian@gmail.com>
2020-04-30 19:54:10 +0700

    fix/pass-basic-fcc-ui-tests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import QuoteBox from '../src/containers/QuoteBox';
 import './App.scss';
-import {BrowserRouter as Router,} from 'react-router-dom';
-
+import { BrowserRouter as Router, } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <div className="app">
-    <div className="main-content-container">
-        <QuoteBox />
-        <div className="developer-credit">Created by Cameron Chardukian</div>
+      <div className="app">
+        <div className="main-content-container">
+          <QuoteBox />
+          <div className="developer-credit">Created by Cameron Chardukian</div>
         </div>
-    </div>
+      </div>
     </Router>
   );
 }
